39e5e019c31afc750e0a47709d657a68790ce6a0
Joe Kim <joesm.genie@gmail.com>
2023-05-12 15:00:17 +0700

    Added get post metadata and show on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,25 @@
+import fs from "fs";
+import Link from "next/link";
+
+const getPostMetadata = () => {
+  const folder = "posts/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
+  return slugs;
+};
+
 const HomePage = () => {
-    return <h1>Hello from NextJS 13</h1>;
-}
+  const postMetadata = getPostMetadata();
+  const postPreviews = postMetadata.map((slug) => (
+    <div>
+      <Link href={`/posts/${slug}`}>
+        <h3>{slug}</h3>
+      </Link>
+    </div>
+  ));
+
+  return <h1>{postPreviews}</h1>;
+};
 
 export default HomePage;
