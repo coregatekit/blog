f19b4288dd7ad773c4cef2295ac29d32cc821b3e
Joe Kim <joesm.genie@gmail.com>
2023-05-12 16:19:47 +0700

    Update css post preview

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -3,11 +3,11 @@ import { PostMetadata } from './PostMetadata';
 
 const PostPreview = (props: PostMetadata) => {
   return (
-    <div className='border border-slate-200 p-4 rounded-md shadow-md bg-white'>
+    <div className='border border-slate-300 p-4 rounded-md shadow-md bg-white'>
       <Link href={`/posts/${props.slug}`}>
         <h2 className='font-bold text-blue-600 hover:underline'>{props.title}</h2>
       </Link>
-      <p className='text-sm text-slate-400'>{props.date}</p>
+      <p className='text-sm text-slate-400 mb-2'>{props.date}</p>
       <p className='text-slate-700'>{props.subtitle}</p>
     </div>
   );
